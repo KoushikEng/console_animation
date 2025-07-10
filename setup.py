920a5ec6b2344b2cbe307edca664d17606258464
Koushik Ghosh <55987395+koushikghosh11@users.noreply.github.com>
2025-07-10 22:25:30 +0530

    Fix: Add long_description from README

--- setup.py
+++ setup.py
@@ -1,11 +1,18 @@
 from setuptools import setup, find_packages
+from pathlib import Path
+
+this_dir = Path(__file__).parent
+long_description = (this_dir / "README.md").read_text(encoding="utf-8")
 
 setup(
     name='console-animation',
-    version='0.1.1',
+    version='0.1.2',
     description='An easy to use decorator to show a console spinner during function execution.',
+    long_description=long_description,
+    long_description_content_type='text/markdown',
     author='Koushik',
     author_email='koushikla115@gmail.com',
+    url='https://github.com/KoushikEng/console-animation',
     packages=find_packages(),
     python_requires='>=3.6',
     classifiers=[
